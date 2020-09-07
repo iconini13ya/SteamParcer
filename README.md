de86afbe3ae7b4a6a2b1bf2b9c850228964e569c
Vladimir <gglobalink@gmail.com>
2020-09-07 18:45:33 +0300

    dd

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-SteamParcer
+# Hi there 👋
+## This is my SteamParcer on NodeJs with using needle and cheerio library
+## To deploy the project you need NodeJs onboard. Copy my project and place your url there.
--- parcer.js
+++ parcer.js
@@ -1,17 +1,49 @@
 var needle= require("needle");
 var cheerio= require("cheerio");
 url="https://steamcommunity.com/market/listings/570/Peregrine%20Flight"
-
+courseURL="http://www.cbr.ru/scripts/XML_daily.asp"
+function getEuroPrice(){
+    return new  Promise ((resolve, reject)=>{
+        needle.get(courseURL,(err,res)=>{
+            resolve(res.body.children[11].children[4].value);
+           });
+    });
+}
 needle.get(url,(err,res)=>{
     if(err) throw err;
     var $= cheerio.load(res.body);
+    // priceArr = Array.from($(".market_listing_price.market_listing_price_with_fee").text().replace(/\s/g, ''));
+    console.log( $(".market_listing_price.market_listing_price_with_fee").text().replace(/\s/g, ' ') );
+    // console.log($);
+});
+// getEuroPrice().then((res)=>{console.log(res);});
 
-   
-    imgs = $(".playerAvatar img");
-    console.log($(".market_listing_price.market_listing_price_with_fee").text());
-    // imgs.each((i,val)=>{
-        
-    //     console.log($(val).attr("src"));
+// var text = "2,11";
+// console.log(text.search('€')); 
 
-    // });
-})
+// needle.get(courseURL,(err,res)=>{
+
+// console.log(res.body.children[11].children[4].value);
+// });
+
+// let Myarr=[1,100];
+// function MidArrMass(arr) {
+//     var Summ=0;
+//     for (let index = 0; index < arr.length; index++) {
+//         Summ=Summ+arr[index];        
+//     }
+//     Summ=Summ/arr.length;
+//     // console.log(Date.now());
+//     return Summ*Math.abs(+Math.sin(Date.now()).toFixed(2));
+// }
+// // console.log(MidArrMass(Myarr).toFixed(2));
+// console.log(MidArrMass(Myarr));
+
+// function FactorN(N) {
+//     var Factor=1;
+//     for (let index = 1; index-1 < N; index++) {
+//         Factor=Factor*index;
+//     }
+//     return Factor;
+// }
+// console.log(FactorN(11));
